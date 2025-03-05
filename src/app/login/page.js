073fa847645fa14619ea1d899072b91d47c83f95
downn_falls <maneeohooho3790@gmail.com>
2025-03-06 00:17:53 +0700

    v1.2

---
+++ src/app/login/page.js
@@ -0,0 +1,12 @@
+import "./loginpage.css"
+
+export default function LoginPage() {
+    return (
+        <div className="layout">
+            <div className="login">
+                <image src=""/>
+                <h1>Ngao Ngao</h1>
+            </div>
+        </div>
+    );
+}
