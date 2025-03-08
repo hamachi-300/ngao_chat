d69de20d29c34476db20e9c7c8a328cf0c4e3091
downn_falls <maneeohooho3790@gmail.com>
2025-03-08 13:53:44 +0700

    remove discord authentication

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,22 +1,25 @@
 import NextAuth from 'next-auth'
-import DiscordProvider from 'next-auth/providers/discord'
 import GoogleProvider from 'next-auth/providers/google'
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import client from "@/database/mongodb"
 
 // https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
-const scopes = ['identify'].join(' ')
 
 const handler = NextAuth({
+
   providers: [
-    DiscordProvider({
-        clientId: process.env.DISCORD_CLIENT_ID,
-        clientSecret: process.env.DISCORD_CLIENT_SECRET,
-        authorization: {params: {scope: scopes}},
-    }),
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        profile(profile) {
+          return {
+            id: profile.sub, 
+            name: profile.name,
+            email: profile.email,
+            image: profile.picture, 
+            username: null
+          };
+        }
     })
   ],
   adapter: MongoDBAdapter(client)
