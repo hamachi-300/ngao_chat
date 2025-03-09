d4bd72f6d7e10ee11fbffc2a9ed6bc2fa8d10b58
Hamachi-300 <noname14463@gmail.com>
2025-03-10 00:51:52 +0700

    notify v1.0 (not completed)

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,9 +1,9 @@
 "use client";
 import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
-import { IoIosNotifications } from "react-icons/io";
 import { useRouter } from "next/navigation";
 import ProfileModal from "./ProfileModal";
+import NotifyModal from "./NotifyModal";
 
 export default function Navbar() {
     const pathname = usePathname();
@@ -20,10 +20,8 @@ export default function Navbar() {
 
             <nav className="bg-indigo-500 fixed text-white px-8 py-4 flex items-center justify-center shadow-lg h-16 top-0 left-0 w-full z-50">
                 
-                <button 
-                    className="transition-all duration-250 text-3xl text-white hover:scale-110 hover:text-yellow-300 hover:drop-shadow-lg focus:outline-none"
-                >
-                    <IoIosNotifications />
+                <button className="absolute left-8">
+                    <NotifyModal session={session}/>
                 </button>
 
                 {/* Centered Logo Text */}
---
+++ src/app/components/NotifyModal.js
@@ -0,0 +1,49 @@
+'use client';
+import React, { useEffect, useState } from "react";
+import { IoIosNotifications } from "react-icons/io";
+
+export default function NotifyModal({ session }) {
+    const [modal, setModal] = useState(false);
+
+    if (!session) return null;
+
+    const toggleModal = () => {
+        setModal(!modal);
+    }
+
+    return (
+        <>
+            <div className="transition-all duration-250 text-3xl text-white hover:scale-110 hover:text-yellow-300 hover:drop-shadow-lg focus:outline-none cursor-pointer" onClick={() => {toggleModal()}}>
+                <IoIosNotifications />
+            </div>
+
+            {modal && (
+                <div onClick={() => toggleModal()} className=" fixed inset-0 flex items-center justify-center z-50 bg-gray-800/70 bg-opacity-60">
+                    <div
+                        // stop another bubbling up event
+                        onClick={(e) => e.stopPropagation()}
+                        className="rounded-xl shadow-2xl w-96 max-w-md bg-[#535C91]"
+                    >
+                        <div className="bg-[#1B1A55] rounded-t-lg p-5 text-2xl">
+                            Notifications
+                        </div>
+                        <div className="bg-[#535C91] rounded-lg p-5 h-[300px] overflow-y-auto">
+                            <div className="pb-5">
+                                <div className="text-2xl text-left">
+                                    Post Content
+                                </div>
+                                <div className="text-lg text-left m-2">
+                                    Comment
+                                </div>
+                                <div className="h-2 w-20/20 bg-[white] rounded-full"></div>
+                            </div>
+                        </div>
+                        <div className="bg-[#E43F5A] rounded-b-lg p-3 text-xl cursor-pointer">
+                            clear
+                        </div>
+                    </div>  
+                </div>
+            )}
+        </>
+    );
+}
