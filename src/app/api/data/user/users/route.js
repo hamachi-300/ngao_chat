896e251a0cc752a6a2e4c98b1b1067a59041aaeb
downn_falls <maneeohooho3790@gmail.com>
2025-03-09 23:45:08 +0700

    author username has been added to post details

--- src/app/api/data/user/users/route.js
+++ src/app/api/data/user/users/route.js
@@ -1,21 +1,28 @@
 import { ObjectId } from "mongodb";
 import clientPromise from "@/database/mongodb";
 
-export async function GET(request, { params }) {
+export async function POST(request) {
+
+    const body = await request.json();
+    const db = (await clientPromise).db(process.env.MONGO_DB);
+    const collection = db.collection('users');
+
+    const objectIds = body.author_ids.map(id => ObjectId.createFromHexString(id));
 
     try {
-        const db = (await clientPromise).db(process.env.MONGO_DB);
-        const collection = db.collection("users");
+        const users = await collection.find({
+            _id: { $in: objectIds }
+        }).toArray();
 
-        console.log((await params).id);
-        
-        const result = await collection.findOne(
-            { _id: ObjectId.createFromHexString((await params).id) }
-        );
+        // Return only the necessary fields: id and username
+        const usersData = users.map(user => ({
+            id: user._id,
+            username: user.username,
+        }));
 
-        return Response.json(result, { status: 200 });
+        return Response.json(usersData, { status: 200 });
     } catch (error) {
-        //console.error(error);
+        console.error("Error fetching users:", error);
         return Response.json({ Error: error }, { status: 500 });
     }
 }
