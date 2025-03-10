a15f536e24d32e490adab313229575e73dfa799f
Hamachi-300 <noname14463@gmail.com>
2025-03-10 14:48:21 +0700

    api fix compatibility

---
+++ src/app/api/data/notify/reset_notify/[user_id]/route.js
@@ -0,0 +1,46 @@
+"use server"
+
+import clientPromise from "@/database/mongodb";
+import { ObjectId } from "mongodb";
+
+// increase notify
+export async function PATCH(request, { params }) {
+    try {
+        // Parse the request body as JSON (make sure content type is JSON)
+        const updateFields = await request.json();
+  
+        if (!updateFields) {
+            return new Response("Invalid input data", { status: 400 });
+        }
+  
+        // Connect to the database
+        const db = (await clientPromise).db(process.env.MONGO_DB);
+        const collection = db.collection("users");
+  
+        // reset the notify field 
+        updateFields.notify = 0;
+  
+        // Perform the update operation
+        const result = await collection.updateOne(
+            { _id: ObjectId.createFromHexString(params.user_id) },
+            { $set: updateFields }
+        );
+  
+        // Check if the update was successful
+        if (result.modifiedCount === 0) {
+            console.log(result)
+            return new Response("No documents updated", { status: 404 });
+        }
+  
+        return new Response(
+            JSON.stringify({ message: "Notify incremented successfully" }),
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error("Database connection error:", error);
+        return new Response(
+            JSON.stringify({ error: "Failed to connect to database" }),
+            { status: 500 }
+        );
+    }
+}
